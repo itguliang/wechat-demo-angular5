7c73cce3b4901d5b406d4986c977aeab4df86997
gfishare <gfishare@gfisharedeMac-mini.local>
2018-01-09 12:49:31 +0800

    update

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,17 +3,17 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const CHATLIST = [
-      { id: 10, name: '路飞',photo:"./assets/lufei.jpg",date:1515467235585},
-      { id: 11, name: '张三',photo:"./assets/lufei.jpg",date:1515467235585},
-      { id: 12, name: '李四',photo:"./assets/lufei.jpg",date:1515467235585},
-      { id: 13, name: '王二',photo:"./assets/lufei.jpg",date:1515467235585},
-      { id: 14, name: 'Celeritas',photo:"./assets/lufei.jpg",date:1515467235585},
-      { id: 15, name: 'Magneta',photo:"./assets/lufei.jpg",date:1515467235585},
-      { id: 16, name: 'RubberMan',photo:"./assets/lufei.jpg",date:1515467235585},
-      { id: 17, name: 'Dynama',photo:"./assets/lufei.jpg",date:1515467235585},
-      { id: 18, name: 'Dr IQ',photo:"./assets/lufei.jpg",date:1515467235585},
-      { id: 19, name: 'Magma' ,photo:"./assets/lufei.jpg",date:1515467235585},
-      { id: 20, name: 'Tornado' ,photo:"./assets/lufei.jpg",date:1515467235585}
+      { id: 10, name: '路飞',photo:"assets/lufei.jpg",date:1515467235585},
+      { id: 11, name: '张三',photo:"assets/lufei.jpg",date:1515467235585},
+      { id: 12, name: '李四',photo:"assets/lufei.jpg",date:1515467235585},
+      { id: 13, name: '王二',photo:"assets/lufei.jpg",date:1515467235585},
+      { id: 14, name: 'Celeritas',photo:"assets/lufei.jpg",date:1515467235585},
+      { id: 15, name: 'Magneta',photo:"assets/lufei.jpg",date:1515467235585},
+      { id: 16, name: 'RubberMan',photo:"assets/lufei.jpg",date:1515467235585},
+      { id: 17, name: 'Dynama',photo:"assets/lufei.jpg",date:1515467235585},
+      { id: 18, name: 'Dr IQ',photo:"assets/lufei.jpg",date:1515467235585},
+      { id: 19, name: 'Magma' ,photo:"assets/lufei.jpg",date:1515467235585},
+      { id: 20, name: 'Tornado' ,photo:"assets/lufei.jpg",date:1515467235585}
     ];
     return {CHATLIST};
   }
